34c6022677aaf1ba2de724ff37d54e9ce92cdb5e
BorysDudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2023-03-23 23:07:31 +0200

    Add files via upload

---
+++ scripts/languageSelect.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const select = document.getElementById('languageSelect');
+        
+          select.addEventListener('change', (event) => {
+            const selectedOption = event.target.value;
+            if (selectedOption === 'uk') {
+              window.location.href = 'index.html'; // перенаправление на страницу на украинском языке
+            } else if (selectedOption === 'en') {
+              window.location.href = 'en.html'; // перенаправление на страницу на английском языке
+            }
+          });
+        });
